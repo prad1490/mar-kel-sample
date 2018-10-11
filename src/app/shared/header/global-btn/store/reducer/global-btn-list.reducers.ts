9e871a088753485a0d0f635327a510692b7e140b
prad1490 <dheep2lrn@gmail.com>
2018-10-11 13:22:43 +0530

    few changes

--- src/app/shared/header/global-btn/store/reducer/global-btn-list.reducers.ts
+++ src/app/shared/header/global-btn/store/reducer/global-btn-list.reducers.ts
@@ -4,19 +4,18 @@ import { DropdownMenuItem } from "../model/global-btn.model";
 
 
 const initialState: DropdownMenuItem = {
-    menuItems: ["Global Re", "chubb", "A", "B", "C", "D"]
+    menuItems: ["Global Re", "CATCO", "Chubb", "Fed Fronted", "MINT", "Markell Assurance"]
 };
-
-export function globalBtnListReducer(state: any = initialState,
+export function globalBtnListReducer(state: DropdownMenuItem = initialState,
     action: SetglobalBtnListActions.SetglobalBtnListActions) {
     switch (action.type) {
         case SetglobalBtnListActions.SET_GRS_DROPDOWN:
             state.menuItems.unshift(action.payload);
-            return [state.menuItems.filter((el, i) => {
-                return state.menuItems.indexOf(el) == i;
-            })];
+            return Object.assign({}, state,
+                [state.menuItems.filter((el, i) => {
+                    return state.menuItems.indexOf(el) == i;
+                })]);
         default:
             return state;
     }
 }
-
